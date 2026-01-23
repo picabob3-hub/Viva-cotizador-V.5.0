7365dbc2682429e1e333023be76fafa8f66067b2
picabob3-hub <picabob3@gmail.com>
2026-01-23 11:15:18 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'viva-estudio-v7';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
